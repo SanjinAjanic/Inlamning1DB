63f6b84b4a6d4ada74021eaca92780fad537eb9b
Sanjin Ajanic <sanjin.ajanic@hotmail.com>
2021-02-15 02:55:09 +0100

    gjort ändringar och lagt till flera metoder och anropat dom i main

--- Inlamning1DB/Databas.cs
+++ Inlamning1DB/Databas.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Text;
 
 namespace Inlamning1DB
@@ -9,7 +10,72 @@ namespace Inlamning1DB
     class Databas
     {
         public string ConnectionString { get; set; } = @"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=true;database={0}";
-        public string DatabaseName { get; set; } = "Persons";
+
+
+        public string DatabaseName { get; set; } = "Familjeträd";
+
+        public void Setup()
+        {
+            if (CreateDatabase() == true)
+            {
+                CreateTable();
+                InsertMembers();
+            }
+        }
+        
+
+        /// <summary>
+        /// Försöker skapa upp Basen Familjeträd
+        /// </summary>
+        /// <returns>true om det gick och skapa databasen och om 
+        /// databasen redan finns så false</returns>
+        public bool CreateDatabase()
+        {            
+            try
+            {
+                DatabaseName = "master";
+                ExecuteQuery("CREATE DATABASE Familjeträd");
+                DatabaseName = "Familjeträd";
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Skapar upp tabellen Familj.
+        /// </summary>
+        internal void CreateTable()
+        {
+            string createTable = "CREATE TABLE Familj (" +
+                "Id int PRIMARY KEY IDENTITY (1,1) NOT NULL, " +
+                "FirstName nvarchar(50) NOT NULL, " +
+                "LastName nvarchar(50) NOT NULL, " +
+                "MotherId int NULL, " +
+                "FatherId int NULL)";
+
+            ExecuteQuery(createTable);          
+        }
+
+        internal void InsertMembers()
+        {
+            var filename = Path.Combine(Environment.CurrentDirectory, @"..\..\..\..\personer.txt");
+            if (File.Exists(filename))
+            {
+                var fileRows = File.ReadAllLines(filename);
+                foreach (var item in fileRows)
+                {
+                    //Console.WriteLine(item);
+                    var split = item.Split(", ");
+                    var person = new Person();
+                    person.FirstName = split[0].Trim();
+                    person.LastName = split[1].Trim();
+                    CreatePerson(person);
+                }
+            }
+        }
 
         public void ExecuteQuery(string sqlString, params (string,string)[] parameters)
         {
@@ -26,6 +92,7 @@ namespace Inlamning1DB
             }
             cnn.Close();
         }
+
         public DataTable ExecuteQueryWithTable(string sqlString, params (string, string)[] parameters)
         {
             var dt = new DataTable(); // förbered Datatable
@@ -47,6 +114,7 @@ namespace Inlamning1DB
             cnn.Close(); // stäng koppling
             return dt; // returnerar datatable
         }
+
         /// <summary>
         /// koppla till databasen
         /// </summary>
@@ -58,23 +126,21 @@ namespace Inlamning1DB
             Console.WriteLine($"Using database: {cnn.Database}");
             return cnn;
         }
+
         public void CreatePerson(Person person)
         {
-            string sql = "insert into PersonT (FirstName, LastName, MotherId, FatherId)" +
+            string sql = "insert into Familj (FirstName, LastName, MotherId, FatherId)" +
                  "values (@FirstName, @LastName, @MotherId, @FatherId)";
             var parameters = new (string, string)[]
             {
                 ("@FirstName", person.FirstName),
                 ("@LastName", person.LastName),
                 ("@MotherId", person.MotherId.ToString()),
-                ("@FatherId", person.FatherId.ToString()),
-             
-
+                ("@FatherId", person.FatherId.ToString()),             
             };
             ExecuteQuery(sql, parameters);
-
-
         }
+
         public void DeletePerson(Person person)
         {
             string sql = "DELETE FROM PersonT Where PersonId=@PersonId";
@@ -91,6 +157,7 @@ namespace Inlamning1DB
             };
             ExecuteQuery(sql, parameters);
         }
+
         public DataTable ReadPerson(Person person)
         {
             string sql = "Select * FROM PersonT Where FirstName=@FirstName OR LastName=@LastName";
@@ -99,11 +166,10 @@ namespace Inlamning1DB
             {
                 ("@FirstName", person.FirstName),
                 ("@LastName", person.LastName),
-              
-
             };
             return ExecuteQueryWithTable(sql, parameters);
         }
+
         public  void UpdatePerson(Person person)
         {
             string sql = "UPDATE PersonT Set FirstName=@FirstName, LastName=@LastName, MotherId=@MotherId, FatherId=@FatherId WHERE PersonId=@PersonId";
@@ -121,6 +187,7 @@ namespace Inlamning1DB
             };
             ExecuteQuery(sql, parameters);
         }
+
         public void SetParents(Person person)
         {
             string sql = "UPDATE PersonT Set MotherId=@MotherId, FatherId=@FatherId WHERE PersonId=@PersonId";
@@ -141,7 +208,7 @@ namespace Inlamning1DB
         public int ReadPersonId(Person person)
         {
 
-            string sql = "Select * FROM PersonT Where FirstName=@FirstName AND LastName=@LastName";
+            string sql = "Select * FROM Familj Where FirstName=@FirstName AND LastName=@LastName";
 
             var parameters = new (string, string)[]
             {
@@ -159,24 +226,71 @@ namespace Inlamning1DB
                 return Id;
             }
             return 0;
-
-
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public List<Person> ReadPerson(string name)
+        {
+            string sql = "SELECT * FROM Familj WHERE FirstName LIKE @FirstName";
+            DataTable dt = ExecuteQueryWithTable(sql, ("@FirstName", $"{name}%"));
+            List<Person> people = new List<Person>();
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    people.Add(ConvertToPerson(row));
+                }               
+            }
+            return people;
+        }
+        // 1. a) gör anrop till databasen of fråga efter alla personer med namnet du fått in. klar!!!
+        // 1. b) kolla om datatable innehåller någon person. klar!!!
+        // 1. c) omvandla varje rad i dt.Rows till ett person-objekt och lägg till i en lista. klar!!!
+        // 2. returnera listan av Person. klar !!!
+        // 3. skriv en sammanfattning om vad metoden gör...
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        private Person ConvertToPerson(DataRow row)
+        {
+            Person person = new Person();
+            person.ID = (int)row["Id"]; 
+            person.FirstName = row["FirstName"].ToString();
+            person.LastName = row["LastName"].ToString();
+            person.MotherId = (int)row["MotherId"];
+            person.FatherId = (int)row["FatherId"];
+            return person;
+        }
+
+        // Ska ta varje kolumn i row och stoppa in i varje prop i person.
+        // 1. Skapa en ny Person och döp den till person. klar!!
+        // 2. Tilldela sedan varje prop i person ett värde från row.
+        //      tex. person.FirstName = row["FirstName"]; klar!!!
+        // 3. returnera person. klar!!!
+        // 4. Skriv en sammanfattning om vad metoden gör!
+
+
+
+        
         public int IfNotExist(Person person)
         {
             var id = ReadPersonId(person);
             if (id == 0)
-            {
-                
+            {                
                 CreatePerson(person);
                 id = ReadPersonId(person);
                 person.ID = id;
-
             }
             return id;
         }
 
     }
-
-
 }
--- Inlamning1DB/Program.cs
+++ Inlamning1DB/Program.cs
@@ -6,35 +6,16 @@ using System.IO;
 namespace Inlamning1DB
 {
     class Program
-
     {
 
         static void Main(string[] args)
-
         {
-
-            var filename = @"C:\Users\sanji\OneDrive\Skrivbord\personer.txt";
-            List<Person> Familj = new List<Person>();
-            Databas db = new Databas();
-            if (File.Exists(filename))
-            {
-                var fileRows = File.ReadAllLines(filename);
-                foreach (var item in fileRows)
-                {
-                    Console.WriteLine(item);
-                    var split = item.Split(',');
-                    var name = split[0].Trim();
-                    var lastName = split[1].Trim();
-                    db.IfNotExist(new Person(name, lastName));
+            var db = new Databas();
+            db.Setup();
+            FindPerson();
 
 
-                }
 
-            }
-            foreach (var item in Familj)
-            {
-                Console.WriteLine(item.FirstName);
-            }
 
 
             /* var id = db.ReadPersonId(new Person { FirstName = "Denana", LastName = "Ajanic" });
@@ -56,33 +37,90 @@ namespace Inlamning1DB
              db.UpdatePerson(new Person { FirstName = "Sanjin", LastName = "Ajanic", ID = Id });
 
              */
-            var dt = db.ReadPerson(new Person { FirstName = "Sanjin", LastName = "Ajanic" });
+
+            //var sanjin = new Person() { FirstName = "Sanjin", LastName = "Ajanic" };
+
+            ////var dt = db.ReadPerson(new Person { FirstName = "Sanjin", LastName = "Ajanic" });
+            //var dt = db.ReadPerson(sanjin);
 
 
-            if (dt.Rows.Count > 0)
-            {
-                foreach (DataRow row in dt.Rows)
-                {
-                    Console.WriteLine($"{row["FirstName"]}  {row["LastName"]} ");
-                }
-            }
+            //if (dt.Rows.Count > 0)
+            //{
+            //    foreach (DataRow row in dt.Rows)
+            //    {
+            //        Console.WriteLine($"{row["FirstName"]}  {row["LastName"]} ");
+            //    }
+            //}
 
-            var Sanjin = db.ReadPersonId(new Person { FirstName = "Sanjin", LastName = "Ajanic" });
-            var Elvedin = db.ReadPersonId(new Person { FirstName = "Elvedin", LastName = "Ajanic" });
-            var Denana = db.ReadPersonId(new Person { FirstName = "Denana", LastName = "Niksic" });
-            db.SetParents(new Person { ID = Sanjin, FatherId = Elvedin, MotherId = Denana });
-            var Asim = db.ReadPersonId(new Person { FirstName = "Asim", LastName = "Ajanic" });
-            var Djulsuma = db.ReadPersonId(new Person { FirstName = "Djulsuma", LastName = "Puzic" });
-            db.SetParents(new Person { ID = Elvedin, FatherId = Asim, MotherId = Djulsuma });
-            var Zenjil = db.ReadPersonId(new Person { FirstName = "Zenjil", LastName = "Niksic" });
-            var Azra = db.ReadPersonId(new Person { FirstName = "Azra", LastName = "Duranovic" });
-            db.SetParents(new Person { ID = Denana, FatherId = Zenjil, MotherId = Azra });
+            ////var Sanjin = db.ReadPersonId(new Person { FirstName = "Sanjin", LastName = "Ajanic" });
+            //var Sanjin = db.ReadPersonId(sanjin);
+            //var Elvedin = db.ReadPersonId(new Person { FirstName = "Elvedin", LastName = "Ajanic" });
+            //sanjin.FatherId = Elvedin;
+            //sanjin.LastName = "Coolman";
+            //var Denana = db.ReadPersonId(new Person { FirstName = "Denana", LastName = "Niksic" });
+            //db.SetParents(new Person { ID = Sanjin, FatherId = Elvedin, MotherId = Denana });
+            //var Asim = db.ReadPersonId(new Person { FirstName = "Asim", LastName = "Ajanic" });
+            //var Djulsuma = db.ReadPersonId(new Person { FirstName = "Djulsuma", LastName = "Puzic" });
+            //db.SetParents(new Person { ID = Elvedin, FatherId = Asim, MotherId = Djulsuma });
+            //var Zenjil = db.ReadPersonId(new Person { FirstName = "Zenjil", LastName = "Niksic" });
+            //var Azra = db.ReadPersonId(new Person { FirstName = "Azra", LastName = "Duranovic" });
+            //db.SetParents(new Person { ID = Denana, FatherId = Zenjil, MotherId = Azra });
 
-        
+
 
 
 
         }
 
+
+        public void AddPerson()
+        {
+            Person person = new Person();
+            Console.Write("Enter a first name: ");
+            person.FirstName = Console.ReadLine();
+            Console.Write("Enter a last name: ");
+            person.LastName = Console.ReadLine();
+            Databas db = new Databas();
+            db.IfNotExist(person);
+        }
+
+        public static void FindPerson()
+        {
+            Console.Write("Enter person to find: ");
+            var name = Console.ReadLine();
+            Databas db = new Databas();
+            List<Person> people = db.ReadPerson(name);
+            if (people.Count > 0)             
+            {
+                int myInt = 1;
+              
+                foreach (var item in people)
+                {
+                    Console.WriteLine(myInt + ". " + item.FirstName + " " + item.LastName);
+                    myInt++;                    
+                }
+                Console.WriteLine("Choose a number");
+                Console.ReadLine();
+
+                // be användaren om en siffra - klar!!!
+                // läs in användarens svar i en variabel.
+                // kolla så att siffran inte är utanför listans gräns
+                // välja ut personen ur listan som stämmer överens med användarens svar.
+                // stoppa personen i en egen Person variabel.
+
+
+
+                // 1. Be användaren om ett namn. Klar!!!
+                // 2. skicka in namnet i ReadPerson och kolla om det finns några personer med det namnet. klar !!!
+                // 3. a) skapa en int som har värdet 1 ovanför foreach-loopen på rad 96. klar!!!
+                // 3. b) skriv ut din int först i cw klar!!!
+                // 3. c) Om listan som kommer tillbaka inte är tom visa personerna i listan. klar!!!
+                // 4. låt användaren välja den personen som har hittats.
+                // 5. om listan är tom skriv ut att det inte fanns någon match.
+
+
+
+            }
+        }
     }
 }
